add3dc202a494b0ea247e5d9fd6582ace7d744cc
franciscotchapua347-eng <franciscotchapua347@gmail.com>
2026-08-13 20:04:53 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-
-function App() {
-  return (
-    <main>
-      <h1>AI App Builder</h1>
-      <p>Crie aplicações com inteligência artificial.</p>
-    </main>
-  );
-}
+import App from "./App";
+import "./styles.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
